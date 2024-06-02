f0ca46d538b7d7d50e97090e7654f3d3efe4185e
te-ing <hi2177@naver.com>
2024-06-02 16:43:47 +0900

    feat: 로컬스토리지를 사용한 Preset CRUD 구현

--- src/component/Preset.tsx
+++ src/component/Preset.tsx
@@ -1,28 +1,127 @@
-import React from 'react';
+/* eslint-disable react/no-unused-prop-types */
+import { useState } from 'react';
 import './Preset.css';
 
+type TPreset = {
+  id: number;
+  title: string;
+  content: string;
+};
 export default function Preset() {
+  const storagePreset: TPreset[] = JSON.parse(
+    localStorage.getItem('preset') || '[]',
+  );
+  const [preset, setPreset] = useState<TPreset[]>(storagePreset);
+  const [editId, setEditId] = useState(0);
+  const [editTitle, setEditTitle] = useState('');
+  const [editContent, setEditContent] = useState('');
+  const addPreset = () => {
+    const key = Date.now() + Math.floor(Math.random() * 999);
+    localStorage.setItem(
+      'preset',
+      JSON.stringify(
+        storagePreset.concat({
+          id: key,
+          title: `Preset ${storagePreset.length + 1}`,
+          content: `contetn ${storagePreset.length + 1}`,
+        }),
+      ),
+    );
+    setPreset((prev) =>
+      prev.concat({
+        id: key,
+        title: `Preset ${storagePreset.length + 1}`,
+        content: `contetn ${storagePreset.length + 1}`,
+      }),
+    );
+  };
+
+  const editPreset = () => {
+    localStorage.setItem(
+      'preset',
+      JSON.stringify(
+        storagePreset.map((target) => {
+          if (editId !== target.id) return target;
+          return {
+            ...target,
+            title: editTitle,
+            content: editContent,
+          };
+        }),
+      ),
+    );
+    setPreset((prev) =>
+      prev.map((target) => {
+        if (editId !== target.id) return target;
+        return {
+          ...target,
+          title: editTitle,
+          content: editContent,
+        };
+      }),
+    );
+    setEditId(0);
+    setEditTitle('');
+    setEditContent('');
+  };
+
+  const deletePreset = (id: number) => {
+    localStorage.setItem(
+      'preset',
+      JSON.stringify(storagePreset.filter((v) => v.id !== id)),
+    );
+    setPreset((prev) => prev.filter((v) => v.id !== id));
+  };
+
   return (
     <div className="wrapper">
-      <div className="itemWrapper">
-        <button type="button" onClick={() => {}}>
-          Preset 1
-        </button>
-        <button className="editBtn" type="button" onClick={() => {}}>
-          ✏️ 수정{' '}
-        </button>
-      </div>
-      <div className="itemWrapper">
-        <button type="button" onClick={() => {}}>
-          Preset 2
-        </button>
-        <button className="editBtn" type="button" onClick={() => {}}>
-          ✏️ 수정{' '}
-        </button>
-      </div>
-      <button className="addBtn" type="button" onClick={() => {}}>
+      {preset?.map(({ title, id }) => {
+        return (
+          <div className="itemWrapper">
+            <button type="button" onClick={() => {}}>
+              {title}
+            </button>
+            <button
+              className="editBtn"
+              type="button"
+              onClick={() =>
+                setEditId((prev) => (prev === 0 || id !== prev ? id : 0))
+              }
+            >
+              ✏️
+            </button>
+            <button
+              className="deleteBtn"
+              type="button"
+              onClick={() => deletePreset(id)}
+            >
+              🗑️
+            </button>
+          </div>
+        );
+      })}
+      <button className="addBtn" type="button" onClick={addPreset}>
         ➕
       </button>
+      {editId !== 0 && (
+        <div className="editWrapper">
+          <div className="editItem">
+            <input
+              placeholder="env title"
+              onChange={(v) => setEditTitle(v.target.value)}
+              value={editTitle}
+            />
+            <input
+              placeholder="env value"
+              onChange={(v) => setEditContent(v.target.value)}
+              value={editContent}
+            />
+          </div>
+          <button className="editConfirmBtn" type="button" onClick={editPreset}>
+            📝
+          </button>
+        </div>
+      )}
     </div>
   );
 }
